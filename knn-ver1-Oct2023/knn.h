adc4d1f88fd53ed4c71db5c8c8c76dd215e295ab
jfred24 <58302405+jfred24@users.noreply.github.com>
2024-01-04 20:45:53 +0000

    knn header

--- knn-ver1-Oct2023/knn.h
+++ knn-ver1-Oct2023/knn.h
@@ -7,7 +7,7 @@
 *	- v0.5, November 2021
 *	- v0.6, October 2023
 *
-*	by João MP Cardoso
+*	by Joï¿½o MP Cardoso
 *	Email: jmpc@fe.up.pt
 *	
 *	SPeCS, FEUP.DEI, University of Porto, Portugal
@@ -20,15 +20,15 @@
 #include "types.h"
 
 
-void copy_k_nearest(BestPoint *dist_points, int num_points,
-		BestPoint *best_points, int k);
+void copy_k_nearest(CLASS_ID_TYPE *dist_points_classification_id, DATA_TYPE *distance_dist_points, int num_points,
+		CLASS_ID_TYPE *bp_classification_id, DATA_TYPE *bp_distance, int k);
 
-void select_k_nearest(BestPoint *dist_points, int num_points, int k);
+void select_k_nearest(CLASS_ID_TYPE *dist_points_classification_id, DATA_TYPE *distance_dist_points, int num_points, int k);
 
-void get_k_NN(Point new_point, Point *known_points, int num_points, BestPoint *best_points,
+void get_k_NN(Point new_point, DATA_TYPE (*kp_features)[NUM_FEATURES], CLASS_ID_TYPE *kp_classification_id, int num_points, CLASS_ID_TYPE *bp_classification_id, DATA_TYPE *bp_distance,
 		int k,  int num_features);
 
-CLASS_ID_TYPE plurality_voting(int k, BestPoint *best_points, int num_classes);
+CLASS_ID_TYPE plurality_voting(int k, CLASS_ID_TYPE *bp_classification_id, DATA_TYPE *bp_distance, int num_classes);
 
 CLASS_ID_TYPE knn_classifyinstance(Point new_point, int k, int num_classes, Point *known_points, int num_points, int num_features);
 
